a3e5d0a01c181f79faa769e1fd34decca2334c6b
drisyaathul <drisyak809@gmail.com>
2023-05-21 15:43:49 +0800

    Added: Display the Board on Console

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -53,7 +53,7 @@ public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("*** Welcome to Tic Tac Toe Game ***");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Player Name : ");
+        System.out.println("Enter the Player Name : ");
         String playerName = scanner.nextLine();
 //UC1:
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
