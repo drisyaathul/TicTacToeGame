0e0a1430333f652698459fbbb6f22c034e210b8e
drisyaathul <drisyak809@gmail.com>
2023-05-21 12:51:27 +0800

    Added : Player and Computer choosing the letter 'X' or 'O'

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     //UC1:
     public char[] CreateEmptyBoard(char[] board) {
@@ -14,11 +16,30 @@ public class TicTacToeGame {
                 board[i] = ' ';
             }
         }
-        System.out.println("Currently No one is playing the Game");
         return board;
+    }
+    //UC2:
+    public char chooseLetter() {
+        /*
+          * Taking input from User to choose either 'X' or 'O'.
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose the Letter 'X' or 'O' :-");
+        char letter = scanner.next().charAt(0);
+        if (letter == 'X' || letter =='O') {
+            System.out.println("Player Selected the Letter.");
+        }else {
+            System.out.println("Invalid Input");
+            letter = chooseLetter();
+        }
+        return letter;
     }
     public static void main(String[] args) {
         System.out.println("*** Welcome to Tic Tac Toe Game ***");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Player Name : ");
+        String playerName = scanner.nextLine();
+//UC1:
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
         /*
          * Create a board of char[] of size 10 and
@@ -26,6 +47,17 @@ public class TicTacToeGame {
          */
         char[] board = new char[10];
         board = ticTacToeGame.CreateEmptyBoard(board);
-
+//UC2:
+        /*
+         * Player and Computer to choose a letter X or O
+         */
+        char playerLetter = ticTacToeGame.chooseLetter();
+        char computerLetter;
+        if (playerLetter == 'X' )
+            computerLetter = 'O';
+        else
+            computerLetter = 'X';
+        System.out.println(playerName+ "'s Letter is "+playerLetter);
+        System.out.println("Computer Player's Letter is "+computerLetter);
     }
 }
